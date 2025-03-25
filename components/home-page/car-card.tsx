86750cc78aa6c3a72b33cdbf60f835c53df00799
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2025-03-25 22:19:58 +0700

    Update new cars

--- components/home-page/car-card.tsx
+++ components/home-page/car-card.tsx
@@ -11,7 +11,7 @@ const CarCard: FC<Props> = ({ car }) => {
   return (
     <Link
       href={`/${car.slug}`}
-      className="transition-transform duration-500 hover:-translate-y-2 rounded-md shadow-md border pt-8 pb-4"
+      className="transition-transform duration-500 hover:-translate-y-2 rounded-md pt-8 pb-4"
     >
       <div className="block relative w-full aspect-video p-10">
         <ContainNextImage src={car.avatar.url} alt={car.name} />
