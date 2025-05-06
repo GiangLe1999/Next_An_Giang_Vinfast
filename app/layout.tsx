225f74b619777a92b348e6068136c946a00576c7
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2025-05-06 10:07:46 +0700

    Add gg tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "@/components/contexts/providers";
-import GoogleAnalytics from "@/components/google-analytics";
 import { Mulish } from "next/font/google";
 import { pageConstants } from "@/data/constants";
+import Script from "next/script";
 
 const mulish = Mulish({
   subsets: ["latin", "vietnamese"],
@@ -24,10 +24,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${mulish.className} antialiased`}>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WCLN98R8');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google Tag Manager - noscript */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCLN98R8"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <Providers>{children}</Providers>
       </body>
-
-      <GoogleAnalytics />
     </html>
   );
 }
--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -6,18 +6,18 @@ const GoogleAnalytics = () => {
       {/* Tải Google Tag Manager */}
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-4QWLZG77RS`}
+        src={`https://www.googletagmanager.com/gtag/js?id=GTM-WCLN98R8`}
       />
 
       {/* Cấu hình Google Analytics */}
-      <Script id="google-tags" strategy="afterInteractive">
+      {/* <Script id="google-tags" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-4QWLZG77RS');
         `}
-      </Script>
+      </Script> */}
     </>
   );
 };
